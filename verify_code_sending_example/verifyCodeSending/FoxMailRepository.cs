4f66b27336b98f71c1d6c269c30a9bfe943ab9dc
lilin9 <lilin0china@gmail.com>
2024-09-09 14:06:06 +0800

    邮箱发送邮件相关的接口

---
+++ verify_code_sending_example/verifyCodeSending/FoxMailRepository.cs
@@ -0,0 +1,61 @@
+﻿using System.Net;
+using System.Net.Mail;
+using System.Text;
+using verifyCodeSending.Repository;
+
+namespace verifyCodeSending {
+    public class FoxMailRepository: IEmailRepository {
+        private const string SmtpServer = "smtp.exmail.qq.com"; //SMTP服务器
+        private const string FromEmail = "lin.li@ebulent.com.cn"; //发信邮箱
+        private const string AuthPwd = "S22i3BnkDGZMNXhd"; //密码或授权码
+
+        /// <summary>
+        /// 发送邮件
+        /// </summary>
+        /// <param name="toEmail">要发往的邮箱</param>
+        /// <param name="subject">邮箱主题</param>
+        /// <param name="msgBody">邮件内容</param>
+        /// <returns></returns>
+        public async Task<bool> SenderEmail(string toEmail, string subject, string msgBody) {
+            if (string.IsNullOrEmpty(SmtpServer) || string.IsNullOrEmpty(FromEmail) ||
+                string.IsNullOrEmpty(AuthPwd)) {
+                return false;
+            }
+
+            using var msg = new MailMessage();
+            /*
+             * msg.To.Add("b.@b.com");  //可以发送给多个人
+             */
+            msg.To.Add(toEmail);    //设置收件人
+
+            /*
+             * msg.CC.Add("c@c.com");
+             * msg.CC.Add("d@d.com"); 可以抄送给多个人
+             */
+
+            //3个参数分别是：发件人地址（可以随便写），发件人姓名，编码
+            msg.From = new MailAddress(FromEmail, FromEmail, System.Text.Encoding.UTF8);
+
+            msg.Subject = subject; //邮件标题
+            msg.SubjectEncoding = Encoding.UTF8; //邮件标题编码
+            msg.Body = msgBody; //邮件内容
+            msg.BodyEncoding = Encoding.UTF8; //邮件内容编码
+            msg.IsBodyHtml = true; //是否是HTML邮件
+            msg.Priority = MailPriority.Normal; //邮件优先级
+
+            var client = new SmtpClient(SmtpServer, 587);  //邮件服务器地址和端口号
+            client.EnableSsl = true;    //使用 ssl 加密发送
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.Credentials = new NetworkCredential(FromEmail, AuthPwd); //邮箱账户、密码
+            client.Timeout = 6000;  //发送邮件6秒超时
+
+            try {
+                await client.SendMailAsync(msg);    //发送邮件
+                return true;
+            } catch (SmtpException e) {
+                Console.WriteLine(e);
+                return false;
+            }
+        }
+    }
+}
---
+++ verify_code_sending_example/webapi/Controllers/TestEmailController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using verifyCodeSending.Repository;
+
+namespace webapi.Controllers {
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class TestEmailController(IEmailRepository emailRepository): ControllerBase {
+        [HttpGet]
+        public async Task<bool> Sender(string email) {
+            return await emailRepository.SenderEmail(email, GetEmailSubject(), GetEmailBody("6666"));
+        }
+
+        /// <summary>
+        /// 获取邮件主题
+        /// </summary>
+        /// <returns></returns>
+        private static string GetEmailSubject() {
+            return "ebulent.com: Here's your Verification Code";
+        }
+
+        /// <summary>
+        /// 获取邮件主题
+        /// </summary>
+        /// <param name="verifyCode"></param>
+        /// <returns></returns>
+        private string GetEmailBody(string verifyCode) {
+            var bodyStr = "<p>If this was you, your verification code is:</p>" +
+                          $"<p style=\"background-color: rgb(211, 211, 211); text-align: left; font-size: 20px; font-weight: bold; font-family: &quot;Amazon Ember&quot;, Arial, sans-serif; padding: 15px 1px 10px 10px; border-radius: 10px; --darkreader-inline-bgcolor: #313537;\">{verifyCode}</p> + " +
+                          "<p>Don’t share it with others.</p>";
+
+            return bodyStr;
+        }
+    }
+}
